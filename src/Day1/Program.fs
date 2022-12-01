041687f64d6b5186c8cdfb3628c084eb5b2ecf48
Charles Farris <charlesfarris71@gmail.com>
2022-11-30 22:44:46 -0700

    Solve part 1

--- src/Day1/Program.fs
+++ src/Day1/Program.fs
@@ -1,6 +1,9 @@
-﻿type ElfCalories = { Id: int; Calories: int }
+﻿open System
+open System.IO
 
-let data =
+type ElfCalories = { Id: int; Calories: int }
+
+let data2 =
     [ 1000
       2000
       3000
@@ -15,6 +18,14 @@ let data =
       9000
       0
       10000 ]
+    
+let parseFun line =
+    if String.IsNullOrWhiteSpace(line) then
+        0
+    else
+        Convert.ToInt32(line)
+let lines = File.ReadLines ".\part1_data.txt" |> List.ofSeq
+let data = List.map parseFun lines 
 
 let elfList: ElfCalories list =
     [ { Id = 1; Calories = 0 } ]
