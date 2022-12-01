4e3c86f2e9da86385638de5981fa81d5342b1574
Charles Farris <charlesfarris71@gmail.com>
2022-11-30 22:32:24 -0700

    Solve test data

--- src/Day1/Program.fs
+++ src/Day1/Program.fs
@@ -1,2 +1,42 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿type ElfCalories = { Id: int; Calories: int }
+
+let data =
+    [ 1000
+      2000
+      3000
+      0
+      4000
+      0
+      5000
+      6000
+      0
+      7000
+      8000
+      9000
+      0
+      10000 ]
+
+let elfList: ElfCalories list =
+    [ { Id = 1; Calories = 0 } ]
+
+let sumFun (acc: ElfCalories list) (elem: int) =
+    let current = acc.Head
+    printfn "%i %i %i" current.Id current.Calories elem
+
+    match elem with
+    | 0 -> List.append [ { Id = current.Id + 1; Calories = 0 } ] acc
+    | _ -> List.append [ { current with Calories = current.Calories + elem } ] acc.Tail
+
+let sumList = List.fold sumFun elfList data
+
+sumList |> List.iter (fun current -> printfn "Id: %i Calories: %i" current.Id current.Calories )
+
+let findMax (acc: ElfCalories ) (elem: ElfCalories) =
+    if acc.Calories < elem.Calories then
+        elem
+    else
+        acc
+
+let max = List.fold findMax { Id = 0; Calories = 0 } sumList
+
+printfn "Max: Id: %i Calories: %i" max.Id max.Calories
