1a27301604a14eff03d939649016c843ad7008c4
Charles Farris <charlesfarris71@gmail.com>
2022-12-01 22:33:43 -0700

    Solve part 2

--- src/Day2/Program.fs
+++ src/Day2/Program.fs
@@ -8,15 +8,15 @@ let strategyGuide =
 
 let playRound (score: int) (line: string): int =
     match line with
-    | "A X" -> score + 1 + 3 // rock-rock 
-    | "A Y" -> score + 2 + 6 // rock-paper
-    | "A Z" -> score + 3 + 0 // rock-scissors
-    | "B X" -> score + 1 + 0 // paper-rock
-    | "B Y" -> score + 2 + 3 // paper-paper
-    | "B Z" -> score + 3 + 6 // paper-scissors
-    | "C X" -> score + 1 + 6 // scissors-rock
-    | "C Y" -> score + 2 + 0 // scissors-paper
-    | "C Z" -> score + 3 + 3 // scissors-scissors
+    | "A X" -> score + 3 + 0 // rock-loss -> scissor
+    | "A Y" -> score + 1 + 3 // rock-draw -> rock
+    | "A Z" -> score + 2 + 6 // rock-win -> paper
+    | "B X" -> score + 1 + 0 // paper-loss -> rock
+    | "B Y" -> score + 2 + 3 // paper-draw -> paper
+    | "B Z" -> score + 3 + 6 // paper-win -> scissor 
+    | "C X" -> score + 2 + 0 // scissors-loss -> paper 
+    | "C Y" -> score + 3 + 3 // scissors-draw -> scissors
+    | "C Z" -> score + 1 + 6 // scissors-win -> rock
     | _ -> score
     
 let score = List.fold playRound 0 strategyGuide
