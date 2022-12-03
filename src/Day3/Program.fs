085b7d4fc17fa271605a9c6d0174b258cd94b9de
Charles Farris <charlesfarris71@gmail.com>
2022-12-02 22:25:57 -0700

    Solve test data

--- src/Day3/Program.fs
+++ src/Day3/Program.fs
@@ -1,2 +1,33 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿let rucksacks: string list =
+    [ "vJrwpWtwJgWrhcsFMMfFFhFp"
+      "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+      "PmmdzqPrVvPwwTWBwg"
+      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+      "ttgJtRGJQctTZtZT"
+      "CrZsJsPPZsGzwwsLwLmpwMDw" ]
+
+let computePriority (c: char) : int =
+    if ('a' <= c && c <= 'z') then
+        int c - 97 + 1
+    elif ('A' <= c && c <= 'Z') then
+        int c - 65 + 27
+    else
+        0
+
+let analyzeCompartments (sum: int) (line: string) =
+    let size = line.Length / 2
+    let left = line[.. (size - 1)]
+    let right = line[size..]
+    printfn "%s %s %s" line left right
+
+    let matches =
+        String.filter right.Contains left |> Seq.toList |> List.distinct
+
+    List.iter (fun c -> printfn "%c" c) matches
+
+    List.fold (fun newSum c -> newSum + (computePriority c)) sum matches
+
+let sum =
+    List.fold analyzeCompartments 0 rucksacks
+
+printfn "Sum: %i" sum
