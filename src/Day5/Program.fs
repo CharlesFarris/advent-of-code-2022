54b5832af1ddad8c9eb9d3e74964eecdcefb7e29
Charles Farris <charlesfarris71@gmail.com>
2022-12-04 23:17:57 -0700

    Solve test data

--- src/Day5/Program.fs
+++ src/Day5/Program.fs
@@ -1,2 +1,72 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let data: string list =
+    [ "    [D]     "
+      "[N] [C]     "
+      "[Z] [M] [P] "
+      " 1   2   3 "
+      " "
+      "move 1 from 2 to 1 "
+      "move 3 from 1 to 3 "
+      "move 2 from 2 to 1 "
+      "move 1 from 1 to 2 " ]
+
+let index =
+    List.findIndex (String.IsNullOrWhiteSpace) data
+
+let stackLines = data[.. (index - 2)]
+List.iter (fun l -> printfn "%s" l) stackLines
+let moveLines = data[(index + 1) ..]
+List.iter (fun l -> printfn "%s" l) moveLines
+
+let numberOfStacks =
+    ((stackLines |> List.last).Length - 2) / 3
+
+printfn "Stacks: %i" numberOfStacks
+
+let stacks: string list list =
+    [ for i in 1..numberOfStacks -> [] ]
+    
+let updateStack (index: int) (line: string) (stack: string list) =
+    let column = 4 * index + 1
+    if line[column] = ' ' then
+        stack
+    else
+        List.append [ line[column] |> string ] stack
+
+let parseStackLine (line: string) (acc: string list list) =
+    [ for i in 0..(numberOfStacks-1) -> updateStack i line acc[i]]
+    
+let stackMap = List.foldBack parseStackLine stackLines stacks
+
+type Move = {
+    Source: int
+    Target: int
+}
+
+let expandMoveLine (acc: Move list) (line : string) =
+    let tokens = line.Split [| ' ' |]
+    let count = tokens[1] |> Int32.Parse
+    let source = tokens[3] |> Int32.Parse
+    let target = tokens[5] |> Int32.Parse
+    List.append acc [for _ in 1..count -> {Source = source; Target = target}]
+    
+let moves = List.fold expandMoveLine [] moveLines
+
+let moveStack (acc: string list list) (move: Move) = 
+    let sourceStack = acc[move.Source - 1]
+    let crate = sourceStack[0]
+    let newSourceStack = List.tail sourceStack
+    let targetStack = acc[move.Target - 1]
+    let newTargetStack = List.append [ crate ] targetStack
+    [for i in 1..acc.Length ->
+        if i = move.Source then
+            newSourceStack
+        elif i = move.Target then
+            newTargetStack
+        else
+            acc[i - 1]]
+    
+let topStacks = List.fold moveStack stackMap moves |> List.fold (fun acc elem -> acc + elem[0]) ""
+
+printfn "Top Stack: %s" topStacks
