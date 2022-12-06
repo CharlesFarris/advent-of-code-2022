5d5dae2971241c1f8f3a0ea158f9d145447a5384
Charles Farris <charlesfarris71@gmail.com>
2022-12-05 22:32:20 -0700

    Solve test data

---
+++ src/Day6/Program.fs
@@ -0,0 +1,7 @@
+﻿let signal = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+
+let chunks = [ for i in 0..(signal.Length - 4) -> signal[i..(i+3)] ] 
+
+let index = chunks |> List.findIndex (fun chunk -> (chunk |> Seq.toList |> List.distinct).Length = 4)
+
+printfn "Index: %i" (index + 4)
