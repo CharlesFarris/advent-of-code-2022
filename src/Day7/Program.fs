90cb50a3a18318a3a7c54e5eef904e833862188c
Charles Farris <charlesfarris71@gmail.com>
2022-12-06 23:32:31 -0700

    Solve tests data

--- src/Day7/Program.fs
+++ src/Day7/Program.fs
@@ -1,2 +1,120 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+
+let input =
+    ".\\test_data.txt"
+    |> File.ReadAllLines
+    |> Seq.toList
+
+type File =
+    { DirectoryName: string
+      Name: string
+      Size: int }
+
+type Directory =
+    { Name: string
+      ParentName: string
+      Depth: int }
+
+type State =
+    { Directories: Map<string, Directory>
+      Files: File list
+      CurrentDirectory: string }
+
+let handleChangeDirectory (name: string) (state: State) : State =
+    match name with
+    | ".." ->
+        { state with
+            CurrentDirectory =
+                state.Directories[state.CurrentDirectory]
+                    .ParentName }
+    | _ -> { state with CurrentDirectory = name }
+
+
+let handleListDirectory (state: State) : State = state
+
+let addDirectory (name: string) (state: State) : State =
+    let depth =
+        match state.CurrentDirectory with
+        | "" -> 0
+        | _ ->
+            state.Directories[state.CurrentDirectory].Depth
+            + 1
+
+    let directory =
+        { Name = name
+          ParentName = state.CurrentDirectory
+          Depth = depth }
+
+    { state with Directories = state.Directories.Add(name, directory) }
+
+let addFile (size: string) (name: string) (state: State) : State =
+    let file =
+        { DirectoryName = state.CurrentDirectory
+          Name = name
+          Size = int size }
+
+    { state with Files = (state.Files |> List.append [ file ]) }
+
+let parseLine (state: State) (line: string) : State =
+    printfn "%s" line
+    let tokens = line.Split [| ' ' |]
+
+    match tokens[0] with
+    | "$" ->
+        match tokens[1] with
+        | "cd" -> state |> handleChangeDirectory tokens[2]
+        | "ls" -> state |> handleListDirectory
+        | _ -> state
+    | "dir" -> state |> addDirectory tokens[1]
+    | _ -> state |> addFile tokens[0] tokens[1]
+
+let initialState: State =
+    { Directories =
+        Map.empty.Add(
+            "/",
+            { Name = "/"
+              ParentName = ""
+              Depth = 0 }
+        )
+      Files = []
+      CurrentDirectory = "" }
+
+let finalState =
+    List.fold parseLine initialState input
+
+let computeDirectorySize (name: string) (state: State) : int =
+    List.fold
+        (fun sum file ->
+            if file.DirectoryName = name then
+                sum + file.Size
+            else
+                sum)
+        0
+        state.Files
+
+let sortedDirectories =
+    finalState.Directories.Values
+    |> Seq.toList
+    |> List.sortBy (fun directory -> -directory.Depth)
+
+let directorySizes: Map<string, int> =
+    List.fold
+        (fun map directory -> map.Add(directory, computeDirectorySize directory finalState))
+        Map.empty
+        (sortedDirectories
+         |> List.map (fun directory -> directory.Name))
+
+let updateParentDirectorySize (map: Map<string, int>) (directory: Directory) =
+    map.Change(
+        directory.ParentName,
+        (fun v ->
+            match v with
+            | Some x -> Some(map[directory.ParentName] + map[directory.Name])
+            | None -> None)
+    )
+
+let totalDirectorySizes = List.fold updateParentDirectorySize directorySizes sortedDirectories
+
+let sum = totalDirectorySizes.Values |> Seq.toList |> List.filter (fun size -> size <= 100000) |> List.sum
+
+printfn "Sum: %i" sum
