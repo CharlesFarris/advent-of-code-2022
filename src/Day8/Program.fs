6bedc6aea5044cac4a7a5bd928545824047c61b8
Charles Farris <charlesfarris71@gmail.com>
2022-12-08 01:02:34 -0700

    Solve part 2

--- src/Day8/Program.fs
+++ src/Day8/Program.fs
@@ -8,33 +8,86 @@ let input =
 let rows = input.Length
 let columns = input[0].Length
 
-let map = Array2D.init rows columns (fun i j -> int( input[i][j])-48)
+let map =
+    Array2D.init rows columns (fun i j -> int (input[i][j]) - 48)
 
-printfn "%A" map 
+printfn "%A" map
 
 let isEdge (row: int) (column: int) (rows: int) (columns: int) : bool =
-    row = 0 || row = rows - 1 || column = 0 || column = columns - 1
-    
-let countVisible (tuple: int*int) (rows: int) (columns: int) (map : int[, ])  : int =
+    row = 0
+    || row = rows - 1
+    || column = 0
+    || column = columns - 1
+
+let computeScenicScore (tuple: int * int) (rows: int) (columns: int) (map: int [,]) : int =
     let row = fst tuple
     let column = snd tuple
-    printfn "%i,%i %i,%i" row column rows columns
-    if (isEdge row column rows columns) then
-        1
-    else
-        let north = [for r in 0..(row-1) -> map[r, column]] |> List.max
-        let south = [for r in (row+1)..(rows-1) -> map[r, column]] |> List.max
-        let west = [for c in 0..(column-1) -> map[row, c]] |> List.max
-        let east = [for c in (column+1)..(columns-1) -> map[row, c]] |> List.max
-        let height = map[row, column]
-        if north < height || south < height || west < height || east < height then
-            1
-        else
-            0
 
-let coords = [for r in 0..(rows-1) ->r ] |> List.collect (fun r -> [for c in 0..(columns-1) -> (r, c)])
+    if (isEdge row column rows columns) then
+        0
+    else
+        let height = map[row, column]
+
+        let northSlice =
+            [ for r in (row - 1) .. -1 .. 0 -> map[r, column] ]
+
+        let northIndex =
+            northSlice
+            |> List.tryFindIndex (fun h -> h >= height)
+
+        let north =
+            match northIndex with
+            | None -> northSlice.Length
+            | Some index -> index + 1
+
+        let southSlice =
+            [ for r in (row + 1) .. (rows - 1) -> map[r, column] ]
+
+        let southIndex =
+            southSlice
+            |> List.tryFindIndex (fun h -> h >= height)
+
+        let south =
+            match southIndex with
+            | None -> southSlice.Length
+            | Some index -> index + 1
+
+        let westSlice =
+            [ for c in (column - 1) .. -1 .. 0 -> map[row, c] ]
+
+        let westIndex =
+            westSlice
+            |> List.tryFindIndex (fun h -> h >= height)
+
+        let west =
+            match westIndex with
+            | None -> westSlice.Length
+            | Some index -> index + 1
+
+        let eastSlice =
+            [ for c in (column + 1) .. (columns - 1) -> map[row, c] ]
+
+        let eastIndex =
+            eastSlice
+            |> List.tryFindIndex (fun h -> h >= height)
+
+        let east =
+            match eastIndex with
+            | None -> eastSlice.Length
+            | Some index -> index + 1
+
+        let scenicScore = north * south * west * east
+    
+        printfn "%i,%i %i,%i -> %i" row column rows columns scenicScore
+        
+        scenicScore
+
+let coords =
+    [ for r in 0 .. (rows - 1) -> r ]
+    |> List.collect (fun r -> [ for c in 0 .. (columns - 1) -> (r, c) ])
 
 printfn "%A" coords
-let count = List.fold (fun c t -> c + countVisible t rows columns map) 0 coords
-        
-printfn "%i" count
+
+let maxScenicScores = coords |> List.map (fun t -> computeScenicScore t rows columns map) |> List.max
+
+printfn "%i" maxScenicScores
