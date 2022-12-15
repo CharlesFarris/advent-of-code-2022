a268ba498780b46e2f410710e1b4d14f81b0e5e5
Charles Farris <charlesfarris71@gmail.com>
2022-12-14 18:23:25 -0700

    Solve part 2

--- src/Day14/Program.fs
+++ src/Day14/Program.fs
@@ -25,8 +25,6 @@ let findMaxColum (path: Cell list) : int =
 let maxRow = paths |> List.fold (fun acc path -> max acc (findMaxRow path)) 0
 let maxColumn = paths |> List.fold (fun acc path -> max acc (findMaxColum path)) 0
 
-let emptyMap =  Map2d.createMap (maxRow + 1) (maxColumn + 1)
-
 let buildPath (path: Cell list) : Cell list =
     let buildPathSegment (s: Cell) (e: Cell) : Cell list = 
         if s.Row = e.Row then
@@ -43,18 +41,14 @@ let buildPath (path: Cell list) : Cell list =
     segments |> List.fold (fun acc s -> s |> List.append acc) []
 
 let rocks = paths |> List.fold (fun acc path -> acc |> List.append (buildPath path)) []
-
-let addRocksToMap (map: Map2d) (cells: Cell list) : Map2d =
-    cells |> List.fold (fun acc elem -> setValue acc elem 1) map
-
-let startingMap = addRocksToMap emptyMap rocks
     
 type State = {
     Units : int
-    Map: Map2d
+    Rocks: Cell list
     IsRunning : bool
     Cell: Cell
     StartCell: Cell
+    Floor: int
 }
 
 let startCell = {Row = 500; Column = 0}
@@ -63,37 +57,36 @@ let initialState = {
     StartCell = startCell 
     Cell = startCell
     Units = 0
-    Map = startingMap
+    Rocks = rocks
     IsRunning = true
+    Floor = maxColumn + 2
 }
 
 let updateState (state: State) : State =
     let moveCell (dr: int) (dc: int) (cell: Cell) : Cell =
         { Row = cell.Row + dr; Column = cell.Column + dc }
-            
-    let down = state.Cell |> moveCell 0 1
 
-    if Map2d.inMap state.Map down then
-        if Map2d.getValue state.Map down = 0 then
-            {state with Cell = down }
-        else
-            let left = state.Cell |> moveCell -1 1
-            if Map2d.inMap state.Map left then
-                if getValue state.Map left = 0 then
-                    {state with Cell = left }
-                else
-                    let right = state.Cell |> moveCell 1 1
-                    if inMap state.Map right then
-                        if getValue state.Map right = 0 then
-                            { state with Cell = right }
-                        else
-                            { state with Map = setValue state.Map state.Cell 1; Cell = startCell; Units = state.Units + 1}
-                    else
-                        {state with IsRunning = false}
-            else
-                {state with IsRunning = false }
+    if state.Cell.Column = state.Floor - 1 then
+        printfn "%i" state.Units
+        { state with Rocks = [ state.Cell ] |> List.append state.Rocks; Cell = startCell; Units = state.Units + 1 }
     else
-        {state with IsRunning = false}
+        let down = state.Cell |> moveCell 0 1
+        if state.Rocks |> List.contains down then
+            let left = state.Cell |> moveCell -1 1
+            if state.Rocks |> List.contains left then
+                let right = state.Cell |> moveCell 1 1
+                if state.Rocks |> List.contains right then
+                    if state.Cell = startCell then
+                        { state with Units = state.Units + 1; IsRunning = false }
+                    else
+                        printfn "%i" state.Units
+                        { state with Rocks = [ state.Cell ] |> List.append state.Rocks; Cell = startCell; Units = state.Units + 1 }
+                else
+                    { state with Cell = right }
+            else
+                { state with Cell = left }
+        else
+            { state with Cell = down }
     
 let rec runSimulation (updateFun : State -> State) (state: State) : State =
     match state.IsRunning with
