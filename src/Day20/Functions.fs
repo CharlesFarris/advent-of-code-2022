0d3d9444f6028a14f6daa8724adfe3bac78dbdbe
Charles Farris <charlesfarris71@gmail.com>
2022-12-19 23:22:21 -0700

    Solve test data

---
+++ src/Day20/Functions.fs
@@ -0,0 +1,33 @@
+﻿module Day20.Functions
+
+let mix (current: int list) (value: int) : int list =
+    if value = 0 then
+        current
+    else
+        let index = current |> List.findIndex (fun i -> i = value)
+        let c1 = current |> List.removeAt index
+        if value > 0 then
+            let absoluteIndex = index + value
+            let relativeIndex = absoluteIndex % current.Length
+            let newIndex =
+                if absoluteIndex < current.Length then
+                    relativeIndex
+                else
+                    relativeIndex + 1
+            let c2 = c1 |> List.insertAt newIndex value
+            c2
+        else
+            let absoluteIndex = index + value - 1
+            let relativeIndex = absoluteIndex % current.Length
+            let newIndex =
+                if absoluteIndex < 0 then
+                    relativeIndex + current.Length
+                else
+                    relativeIndex
+            let c2 = c1 |> List.insertAt newIndex value
+            c2
+    
+let printArrangement (current: int list) : unit =
+    for i in current do
+        printf "%i " i
+    printfn ""
--- src/Day20/Program.fs
+++ src/Day20/Program.fs
@@ -1,2 +1,25 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+open Day20.Functions
+
+let initial =
+    ".\\test_data.txt"
+    |> File.ReadLines
+    |> Seq.map int
+    |> Seq.toList
+    
+let mutable stack = initial |> List.append []
+let mutable current = initial |> List.append []
+current |> printArrangement
+while stack.Length > 0 do
+    let head = stack.Head
+    stack <- stack.Tail
+    current <- head |> mix current
+    current |> printArrangement
+
+let index = current |> List.findIndex (fun i -> i = 0)
+let x = current[ (index + 1000) % current.Length ]
+let y = current[ (index + 2000) % current.Length ]
+let z = current[ (index + 3000) % current.Length ]
+
+let sum = x + y + z
+printfn "Sum: %i" sum
