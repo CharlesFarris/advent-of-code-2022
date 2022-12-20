0182c7bff98da57aaba51a742373e998f2ba0ec9
Charles Farris <charlesfarris71@gmail.com>
2022-12-20 10:47:54 -0700

    Refactor mix algorithm

--- src/Day20/Functions.fs
+++ src/Day20/Functions.fs
@@ -1,5 +1,9 @@
 ﻿module Day20.Functions
 
+let getValueAt (offset: int) (current: int list) : int =
+    let zeroIndex = current |> List.findIndex (fun i -> i = 0)
+    current[ (zeroIndex + offset) % current.Length ]
+
 let mix (current: int list) (value: int) : int list =
     if value = 0 then
         current
@@ -26,8 +30,33 @@ let mix (current: int list) (value: int) : int list =
                     relativeIndex
             let c2 = c1 |> List.insertAt newIndex value
             c2
+            
+let mix2 (current: int list) (value: int) : int list =
+    if value = 0 then
+        current
+    else
+        let index = current |> List.findIndex (fun i -> i = value)
+        if value > 0 then
+            let absoluteIndex = index + value
+            let relativeIndex = absoluteIndex % current.Length
+            let targetValue = current[ relativeIndex ]
+            let c1 = current |> List.removeAt index        
+            let newIndex = 1 + (c1 |> List.findIndex (fun i -> i = targetValue))
+            c1 |> List.insertAt newIndex value
+        else
+            let absoluteIndex = index + value
+            let relativeIndex =
+                if absoluteIndex > 0 then
+                    absoluteIndex % current.Length
+                else
+                    absoluteIndex % current.Length + current.Length
+            let targetValue = current[ relativeIndex - 1]
+            let c1 = current |> List.removeAt index        
+            let newIndex = 1 + (c1 |> List.findIndex (fun i -> i = targetValue))
+            c1 |> List.insertAt newIndex value
     
 let printArrangement (current: int list) : unit =
     for i in current do
         printf "%i " i
     printfn ""
+    
--- src/Day20/Program.fs
+++ src/Day20/Program.fs
@@ -2,24 +2,24 @@
 open Day20.Functions
 
 let initial =
-    ".\\test_data.txt"
+    ".\\part1_data.txt"
     |> File.ReadLines
     |> Seq.map int
     |> Seq.toList
     
 let mutable stack = initial |> List.append []
 let mutable current = initial |> List.append []
-current |> printArrangement
+//current |> printArrangement
 while stack.Length > 0 do
     let head = stack.Head
     stack <- stack.Tail
-    current <- head |> mix current
-    current |> printArrangement
+    current <- head |> mix2 current
+    //current |> printArrangement
 
 let index = current |> List.findIndex (fun i -> i = 0)
-let x = current[ (index + 1000) % current.Length ]
-let y = current[ (index + 2000) % current.Length ]
-let z = current[ (index + 3000) % current.Length ]
+let x = current |> getValueAt 1000
+let y = current |> getValueAt 2000
+let z = current |> getValueAt 3000
 
 let sum = x + y + z
 printfn "Sum: %i" sum
